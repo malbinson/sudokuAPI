f84b674e0cfcc68394a52667380bdf98fac90b64
malbinson <mattalbinson@berkeley.net>
2019-03-12 17:23:42 -0700

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+var express    = require('express')
+var app = express()
+var request = require('request')
+var cors = require('cors');
+
+app.use(cors({origin: 'http://localhost:63342'}));
+
+
+
+
+app.listen(3000, () => {
+    console.log('meow')
+})
+
+app.get('/',function(req,res) {
+  var size = req.query.size
+  var level = req.query.level
+  request('http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=' + size + '&level=' + level, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(body) // Print the google web page.
+          res.set('Content-Type','text/json')
+          res.json(JSON.parse(body))
+       }
+  })
+
+
+})
